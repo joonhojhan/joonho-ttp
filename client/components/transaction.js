def52bc01bc72e5738df57907d3b1fc9e2160a98
Joonho Han <joonhojhan@gmail.com>
2020-02-23 15:30:46 -0500

    adds transactions page with table of all transactions

--- client/components/transaction.js
+++ client/components/transaction.js
@@ -6,8 +6,87 @@ function Transaction(props) {
   React.useEffect(() => {
     props.me()
   }, [])
-  const {user} = props
-  return <div>HELLO</div>
+  const {transactions} = props.user
+  return transactions !== undefined && transactions.length ? (
+    <table className="width-100 text-center">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Ticker Symbol</th>
+          <th scope="col">Company Name</th>
+          <th scope="col">Quantity</th>
+          <th scope="col">Price</th>
+          <th scope="col">Total</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th>
+            {transactions.map(trans => {
+              return (
+                <div key={trans.id}>
+                  {trans.id}
+                  <br />
+                </div>
+              )
+            })}
+          </th>
+          <td>
+            {transactions.map(trans => {
+              return (
+                <div key={trans.id}>
+                  {trans.tickerSymbol}
+                  <br />
+                </div>
+              )
+            })}
+          </td>
+          <td>
+            {transactions.map(trans => {
+              return (
+                <div key={trans.id}>
+                  {trans.company}
+                  <br />
+                </div>
+              )
+            })}
+          </td>
+          <td>
+            {transactions.map(trans => {
+              return (
+                <div key={trans.id}>
+                  {trans.shares}
+                  <br />
+                </div>
+              )
+            })}
+          </td>
+          <td>
+            {transactions.map(trans => {
+              return (
+                <div key={trans.id}>
+                  ${(trans.price / 100).toFixed(2)}
+                  <br />
+                </div>
+              )
+            })}
+          </td>
+          <td>
+            {transactions.map(trans => {
+              return (
+                <div key={trans.id}>
+                  ${((trans.price * trans.shares) / 100).toFixed(2)}
+                  <br />
+                </div>
+              )
+            })}
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  ) : (
+    <div className="text-center">No orders made!</div>
+  )
 }
 
 const mapStateToProps = state => ({
